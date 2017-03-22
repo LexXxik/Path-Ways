73d3d6322dd83bfde60c045df969b679d5f7d651
Adrian Bartko <bartko.adrian@gmail.com>
2017-03-22 21:50:00 +0200

    Projekt

---
+++ EM_App/Assets/Scripts/Dont.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dont : MonoBehaviour
+{
+
+    static Dont Instance;
+
+    void Start()
+    {
+        if (Instance != null)
+            GameObject.Destroy(this.gameObject);
+        else
+        {
+            DontDestroyOnLoad(transform.gameObject);
+            Instance = this;
+        }
+    }
+
+}
